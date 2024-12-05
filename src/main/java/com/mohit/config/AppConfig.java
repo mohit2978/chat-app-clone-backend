0aeeae0c97ffad8dfdf39b9f372a488a58133b04
mohit <mohitrakheja007@gmail.com>
2024-12-05 22:15:17 +0530

    spring security needs to be added

---
+++ src/main/java/com/mohit/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.mohit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AppConfig {
+
+}
