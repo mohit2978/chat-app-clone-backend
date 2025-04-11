cfbe31846918652cb5e350dd2b6b8903db1541ce
mohit <mohitrakheja007@gmail.com>
2025-04-11 17:47:00 +0530

    basic setup done

--- src/main/java/com/mohit/WhatsappApplication.java
+++ src/main/java/com/mohit/WhatsappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WhatsappApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WhatsappApplication.class, args);
 	}
 
